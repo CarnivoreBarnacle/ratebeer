957f2559a39be8a228386f6be3cbc3ad6127a667
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-02-20 22:30:07 +0200

    Fixed a failing test

--- spec/features/places_spec.rb
+++ spec/features/places_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 describe "Places" do
-  	it "if one is returned by the API, it is shown at the page" do
+
+	before :each do
 		allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
       	[ Place.new( name:"Oljenkorsi", id: 1 ) ]
     	)
+    	
+    	allow(BeermappingApi).to receive(:places_in).with("test").and_return(
+      	[ Place.new( name:"Oljenkorsi", id: 1 ), Place.new( name:"Test2", id: 2), Place.new( name:"Test3", id: 3) ]
+    	)
+    	
+    	allow(BeermappingApi).to receive(:places_in).with("empty").and_return(
+      	[ ]
+    	)
+	end
 
+  	it "if one is returned by the API, it is shown at the page" do
     	visit places_path
     	fill_in('city', with: 'kumpula')
     	click_button "Search"
@@ -14,12 +25,8 @@ describe "Places" do
 	end
 	
 	it "if multiple are returned by the API, all are shown at the page" do
-		allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
-      	[ Place.new( name:"Oljenkorsi", id: 1 ), Place.new( name:"Test2", id: 2), Place.new( name:"Test3", id: 3) ]
-    	)
-    	
     	visit places_path
-    	fill_in('city', with: 'kumpula')
+    	fill_in('city', with: 'test')
     	click_button "Search"
 
     	expect(page).to have_content "Oljenkorsi"
@@ -29,9 +36,9 @@ describe "Places" do
 	
 	it "if none are returned by the API, message is shown" do
 		visit places_path
-    	fill_in('city', with: 'kumpula')
+    	fill_in('city', with: 'empty')
     	click_button "Search"
     	
-    	expect(page).to have_content "No locations in kumpula"
+    	expect(page).to have_content "No locations in empty"
 	end
 end
