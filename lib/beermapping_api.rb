6bae688b7ffbb18c3cc23df7dd7b17bc84352e5b
CarnivoreBarnacle <antti.saikkonen@hotmail.fi>
2016-02-20 22:13:45 +0200

    Week 5: excersises 1,2,3 done

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,18 @@
+class BeermappingApi
+
+	def self.places_in(city)
+		#url = "http://beermapping.com/webservice/loccity/#{key}/"
+		url = "http://stark-oasis-9187.herokuapp.com/api/#{ERB::Util.url_encode(city)}"
+
+    	response = HTTParty.get url
+    	places = response.parsed_response["bmp_locations"]["location"]
+
+    	return [] if places.is_a?(Hash) and places['id'].nil?
+
+    	places = [places] if places.is_a?(Hash)
+    	places.map do | place |
+      	Place.new(place)
+    	end
+	end
+
+end
